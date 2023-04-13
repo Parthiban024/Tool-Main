f96740551c3f41eae8cb21d542ca56035ff6f947
Parthiban024 <114136939+Parthiban024@users.noreply.github.com>
2023-04-13 16:00:16 +0530

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const nodemailer = require("nodemailer");
-const router = require('./routes/router');
+const router = require('../IT-tool/backend/routes/router');
 const path = require('path')
 require('dotenv').config(); // load environment variables from .env file
 
